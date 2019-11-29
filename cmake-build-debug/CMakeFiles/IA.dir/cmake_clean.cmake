a6ba18731d5f5704cb2c3c0a36ad7fa96e5aae81
yassinegoeller <yassine.goeller1@outlook.fr>
2019-11-29 13:55:26 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IA.dir/main.cpp.o"
+  "IA"
+  "IA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
