979349f1b9344bb8e75c77bb67692580dbc7ac82
Victor <victor.brungard@gmail.com>
2019-12-02 10:36:52 +0100

    Edit methode class SetUpParams (.cpp)

--- src/DEA.cpp
+++ src/DEA.cpp
@@ -2,3 +2,25 @@
 // Created by Yassine Goeller on 29/11/2019.
 //
 
+void independent_runs(const unsigned int val);
+void nb_evolution_steps(const unsigned int val);
+void population_size(const unsigned int val);
+void solution_size(const unsigned int val);
+
+
+SetUpParams::independent_runs(const unsigned int val){
+    _independent_runs = val;
+}
+
+SetUpParams::nb_evolution_steps(const unsigned int val){
+    _nb_evolution_steps = val;
+}
+
+SetUpParams::population_size(const unsigned int val){
+    _population_size = val;
+}
+
+SetUpParams::solution_size(const unsigned int val){
+    _solution_size = val;
+}
+
