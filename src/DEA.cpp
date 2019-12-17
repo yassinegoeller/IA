54876e35a9c7e3e712d25a8227689789e36bc39f
yassinegoeller <yassine.goeller1@outlook.fr>
2019-12-17 11:15:53 +0100

    Modification DEA.h Fin de l'implementation de SetUpParams

--- src/DEA.cpp
+++ src/DEA.cpp
@@ -127,21 +127,43 @@ void Solution::set_position_in_solution(const int index, const double value)
 
 //=======================================================================
 
-void SetUpParams::independent_runs(const unsigned int val){
+SetUpParams::SetUpParams() : _independent_runs{30}, _nb_evolution_steps{50000},
+_population_size{40}, _solution_size{20}{}
+
+SetUpParams::~SetUpParams()
+{}
+
+const unsigned int SetUpParams::independent_runs() const {
+    return _independent_runs;
+}
+
+const unsigned int SetUpParams::nb_evolution_steps() const {
+    return _nb_evolution_steps;
+}
+
+const unsigned int SetUpParams::population_size() const {
+    return _population_size;
+}
+
+const unsigned int SetUpParams::solution_size() const {
+    return _solution_size;
+}
+
+void SetUpParams::independent_runs(const unsigned int val) {
     _independent_runs = val;
 }
 
-void SetUpParams::nb_evolution_steps(const unsigned int val){
+void SetUpParams::nb_evolution_steps(const unsigned int val) {
     _nb_evolution_steps = val;
 }
 
-void SetUpParams::population_size(const unsigned int val){
+void SetUpParams::population_size(const unsigned int val) {
     _population_size = val;
 }
 
-void SetUpParams::solution_size(const unsigned int val){
+void SetUpParams::solution_size(const unsigned int val) {
     _solution_size = val;
 }
 
-
 //=======================================================================
+
